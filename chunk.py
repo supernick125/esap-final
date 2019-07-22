75a85f67f2beafc96521a387e79ab9c4afb3b3e4
supernick125 <nickhealy33@gmail.com>
2019-07-22 16:34:34 -0400

    Added bedrock

--- chunk.py
+++ chunk.py
@@ -1,16 +1,19 @@
 from grass import *
 from dirt import *
+from bedrock import *
 
 class Chunk:
     def __init__(self):
         self.blocks = []
         for x in range(3):
             for z in range(3):
-                for y in range(3):
+                for y in range(4):
                     if y == 0:
                         self.blocks.append(Grass((x,-y,z)))
-                    else:
+                    elif y >= 1 and y < 3:
                         self.blocks.append(Dirt((x,-y,z)))
+                    elif y == 3:
+                        self.blocks.append(Bedrock((x,-y,z)))
 
     def draw(self):
         for block in self.blocks:
