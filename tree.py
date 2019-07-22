1111e7f643e46da6e3af66a2f6f52f6d1c1d957a
briggsr20 <bobbybriggs3@gmail.com>
2019-07-22 18:41:33 -0400

    more updates for leaf

--- tree.py
+++ tree.py
@@ -6,9 +6,16 @@ class Tree:
 		self.blocks = []
 		for y in range(5):
 			self.blocks.append(Spruce((1,y,1)))
-		for z in range(10):
-			self.blocks.append(Leaves((z, z, 1)))
+		for z in range(6):
+			self.blocks.append(Leaves((z, 5, 1)))
+			self.blocks.append(Leaves((1, z, 1)))
+		for x in range(7):
+			self.blocks.append(Leaves((1, 5, x)))
+			self.blocks.append(Leaves((1, 5, x*(-1))))
 
+
+			
+			
 
 
 	def draw(self):
