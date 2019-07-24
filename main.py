c6e8db9f95ceddb286bed7febd9ad11e5da0b11a
supernick125 <nickhealy33@gmail.com>
2019-07-24 11:27:40 -0400

    Remade Player

--- main.py
+++ main.py
@@ -14,11 +14,19 @@ TICKS_PER_SEC = 60
 
 class Window(pyglet.window.Window):
 
-    def push(self,pos,rot): glPushMatrix(); glRotatef(-rot[0],1,0,0); glRotatef(-rot[1],0,1,0); glTranslatef(-pos[0],-pos[1],-pos[2])
+    def push(self,pos,rot):
+        glPushMatrix()
+        glRotatef(rot[0],0,1,0)
+        glRotatef(-rot[1], math.cos(math.radians(rot[0])), 0, math.sin(math.radians(rot[0])))
+        glTranslatef(-pos[0],-pos[1],-pos[2])
 
-    def Projection(self): glMatrixMode(GL_PROJECTION); glLoadIdentity()
-    def Model(self): glMatrixMode(GL_MODELVIEW); glLoadIdentity()
+    def Projection(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
 
+    def Model(self):
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
 
     def set2d(self):
         glDisable(GL_DEPTH_TEST)
@@ -28,14 +36,13 @@ class Window(pyglet.window.Window):
 
         viewport = self.get_viewport_size()
         glViewport(0, 0, max(1, viewport[0]), max(1, viewport[1]))
-        
-       
+
         self.Projection()
         gluOrtho2D(0,self.width,0,self.height)
         self.Model()
 
 
-    def set3d(self): 
+    def set3d(self):
         glEnable(GL_DEPTH_TEST)
 
         width = self.get_size()
@@ -44,13 +51,16 @@ class Window(pyglet.window.Window):
         viewport = self.get_viewport_size()
         glViewport(0, 0, max(1, viewport[0]), max(1, viewport[1]))
 
-        self.Projection(); 
+        self.Projection();
         gluPerspective(65.0, self.width / self.height, 0.1, 60.0);
         self.Model()
 
-
-    def setLock(self,state): self.lock = state; self.set_exclusive_mouse(state)
-    lock = False; mouse_lock = property(lambda self: self.lock,setLock)
+    #FOR LOCKING MOUSE
+    def setLock(self,state):
+        self.lock = state
+        self.set_exclusive_mouse(state)
+    lock = False
+    mouse_lock = property(lambda self: self.lock,setLock)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -68,16 +78,19 @@ class Window(pyglet.window.Window):
         if self.mouse_lock and BUTTON == mouse.LEFT: self.player.mouse_press(x,y,BUTTON)
 
     def on_mouse_motion(self,x,y,dx,dy):
-
         if self.mouse_lock: self.player.mouse_motion(dx,dy)
 
     def on_key_press(self,KEY,MOD):
         if KEY == key.ESCAPE: self.close()
         elif KEY == key.E: self.mouse_lock = not self.mouse_lock
+        self.player.key_press(KEY,MOD)
+
+    def on_key_release(self,KEY,MOD):
+        self.player.key_release(KEY,MOD)
 
     def update(self,dt):
+        dt = min(dt, 0.2)
         self.player.update(dt,self.keys)
-
 
     def on_draw(self):
         self.clear()
@@ -87,8 +100,6 @@ class Window(pyglet.window.Window):
         self.push(self.player.pos,self.player.rot)
         self.world.draw()
         glPopMatrix()
-
-        
 
 def main():
     window = Window(width=1200, height=600, caption='MINECRAFT?', resizable=True)
--- player.py
+++ player.py
@@ -1,12 +1,22 @@
 from pyglet.window import key
 from pyglet.window import mouse
 import math
-#get coords function
+
+WALKING_SPEED = 10
+
+GRAV = 20.0
+MAX_JUMP = 1.0
+JUMP_SPEED = math.sqrt(2 * GRAV * MAX_JUMP)
+TERMINAL_VEL = 50
+
+PLAYER_HEIGHT = 2
 
 class Player:
     def __init__(self,pos=(0,0,0),rot=(0,0)):
-        self.pos = list(pos)
-        self.rot = list(rot)
+        self.pos = pos
+        self.rot = rot
+        self.lat = [0,0] #lateral movement
+        self.dy = 0 #vertical velocity
 
     def fire(self):
         print("pew")
@@ -15,18 +25,83 @@ class Player:
         return self.fire()
 
     def mouse_motion(self,dx,dy):
-        dx/=8; dy/=8; self.rot[0] += dy; self.rot[1] -= dx
-        if self.rot[0]>90: self.rot[0] = 90
-        elif self.rot[0]<-90: self.rot[0] = -90
+        m = 0.15
+        x,y = self.rot
+        x,y = x + dx * m, y + dy * m
+        y = max(-90, min(90, y))
+        self.rot = (x, y)
+
+    def key_press(self,KEY,MOD):
+        if KEY == key.W:
+            self.lat[0] -= 1
+        elif KEY == key.S:
+            self.lat[0] += 1
+        elif KEY == key.A:
+            self.lat[1] -= 1
+        elif KEY == key.D:
+            self.lat[1] += 1
+        elif KEY == key.SPACE:
+            if self.dy == 0:
+                self.dy = JUMP_SPEED
+
+    def key_release(self,KEY,MOD):
+        if KEY == key.W:
+            self.lat[0] += 1
+        elif KEY == key.S:
+            self.lat[0] -= 1
+        elif KEY == key.A:
+            self.lat[1] += 1
+        elif KEY == key.D:
+            self.lat[1] -= 1
+
+    def get_sight_vector(self):
+        x,y = self.rot
+
+        q = math.cos(math.radians(y))
+        dx = math.cos(math.radians(x - 90)) * q
+        dy = math.sin(math.radians(y))
+        dz = math.sin(math.radians(x - 90)) * q
+
+        return (dx,dy,dz)
+
+    def get_motion_vector(self):
+        if any(self.lat):
+            x,y = self.rot
+            lat = math.degrees(math.atan2(*self.lat))
+            x_angle = math.radians(x + lat)
+            y_angle = math.radians(y)
+
+            #flying?
+
+            dx = math.cos(x_angle)
+            dy = 0.0
+            dz = math.sin(x_angle)
+        else:
+            dx = 0.0
+            dy = 0.0
+            dz = 0.0
+        return (dx,dy,dz)
 
     def update(self,dt,keys):
-        s = dt*10
-        rotY = -self.rot[1]/180*math.pi
-        dx,dz = s*math.sin(rotY),s*math.cos(rotY)
-        if keys[key.W]: self.pos[0] += dx; self.pos[2] -= dz
-        if keys[key.S]: self.pos[0] -= dx; self.pos[2] += dz
-        if keys[key.A]: self.pos[0] -= dz; self.pos[2] -= dx
-        if keys[key.D]: self.pos[0] += dz; self.pos[2] += dx
+        speed = WALKING_SPEED
+        d = dt*speed
+        dx,dy,dz = self.get_motion_vector()
+        dx,dy,dz = dx * d, dy * d, dz * d
 
-        if keys[key.SPACE]: self.pos[1] += s
-        if keys[key.LSHIFT]: self.pos[1] -= s
+        #GRAVITY
+        # self.dy -= dt * GRAV
+        # self.dy = max(self.dy, -TERMINAL_VEL)
+        # dy += self.dy * dt
+
+        x,y,z = self.pos
+        x,y,z = self.collide((x + dx,y + dy,z + dz), PLAYER_HEIGHT)
+        self.pos = (x,y,z)
+
+        rotY = -self.rot[1]/180*math.pi
+        dx,dz = d*math.sin(rotY),d*math.cos(rotY)
+
+    def collide(self, pos, height):
+        p = list(pos)
+
+
+        return tuple(p)
