9fc0bb6d8528b759dabb1ea28cae10fdb59f73a7
briggsr20 <bobbybriggs3@gmail.com>
2019-07-24 11:18:57 -0400

    added fog, fixed main logic

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from pyglet.window import key, mouse
 from player import *
 from world import *
 
+TICKS_PER_SEC = 60
 #collision updates
 
 class Window(pyglet.window.Window):
@@ -17,8 +18,36 @@ class Window(pyglet.window.Window):
 
     def Projection(self): glMatrixMode(GL_PROJECTION); glLoadIdentity()
     def Model(self): glMatrixMode(GL_MODELVIEW); glLoadIdentity()
-    def set2d(self): self.Projection(); gluOrtho2D(0,self.width,0,self.height); self.Model
-    def set3d(self): self.Projection(); gluPerspective(70,self.width/self.height,0.05,1000); self.Model
+
+
+    def set2d(self):
+        glDisable(GL_DEPTH_TEST)
+
+        width = self.get_size()
+        height = self.get_size()
+
+        viewport = self.get_viewport_size()
+        glViewport(0, 0, max(1, viewport[0]), max(1, viewport[1]))
+        
+       
+        self.Projection()
+        gluOrtho2D(0,self.width,0,self.height)
+        self.Model()
+
+
+    def set3d(self): 
+        glEnable(GL_DEPTH_TEST)
+
+        width = self.get_size()
+        height = self.get_size()
+
+        viewport = self.get_viewport_size()
+        glViewport(0, 0, max(1, viewport[0]), max(1, viewport[1]))
+
+        self.Projection(); 
+        gluPerspective(65.0, self.width / self.height, 0.1, 60.0);
+        self.Model()
+
 
     def setLock(self,state): self.lock = state; self.set_exclusive_mouse(state)
     lock = False; mouse_lock = property(lambda self: self.lock,setLock)
@@ -30,7 +59,7 @@ class Window(pyglet.window.Window):
         self.set_mouse_cursor(self.cursor)
         self.keys = key.KeyStateHandler()
         self.push_handlers(self.keys)
-        pyglet.clock.schedule(self.update)
+        pyglet.clock.schedule_interval(self.update, 1.0 / TICKS_PER_SEC)
 
         self.world = World()
         self.player = Player((0.5,2,2),(0,-90))
@@ -39,6 +68,7 @@ class Window(pyglet.window.Window):
         if self.mouse_lock and BUTTON == mouse.LEFT: self.player.mouse_press(x,y,BUTTON)
 
     def on_mouse_motion(self,x,y,dx,dy):
+
         if self.mouse_lock: self.player.mouse_motion(dx,dy)
 
     def on_key_press(self,KEY,MOD):
@@ -48,18 +78,35 @@ class Window(pyglet.window.Window):
     def update(self,dt):
         self.player.update(dt,self.keys)
 
+
     def on_draw(self):
         self.clear()
+        self.set2d()
         self.set3d()
+
         self.push(self.player.pos,self.player.rot)
         self.world.draw()
         glPopMatrix()
 
+        
+
 def main():
-    window = Window(width=1500,height=700,caption="Minecraft",resizable=True)
-    glClearColor(0.5,0.7,1,1)
-    glEnable(GL_DEPTH_TEST)
+    window = Window(width=1200, height=600, caption='MINECRAFT?', resizable=True)
+    glEnable(GL_FOG)
+    glClearColor(0.5, 0.69, 1.0, 1)
     glEnable(GL_CULL_FACE)
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+
+    glEnable(GL_FOG)
+    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.5, 0.69, 1.0, 1))
+    glHint(GL_FOG_HINT, GL_DONT_CARE)
+    glFogi(GL_FOG_MODE, GL_LINEAR)
+    glFogf(GL_FOG_START, 15.0)
+    glFogf(GL_FOG_END, 55.0)
+
+
     pyglet.app.run()
 
 if __name__ == "__main__":
